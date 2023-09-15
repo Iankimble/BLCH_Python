6d7b56d50739e81c995da49d7b670c4eab1751f2
Ian <38480036+Iankimble@users.noreply.github.com>
2023-09-15 16:54:07 +0000

    functions lesson

--- introPythonActivities/readingActivity.py
+++ introPythonActivities/readingActivity.py
@@ -1,2 +1,21 @@
 # read the following articles on Python functions
 "https://www.freecodecamp.org/news/functions-in-python-a-beginners-guide"
+
+# In your own words, describe what a function?
+
+#examples of functions
+print()
+input()
+
+# What does the return keyword do?
+
+# What is a an argument in a function?
+
+# What is parameter in a function?
+
+# What is a method in a function?
+
+
+
+
+
