649791293ebb858281a6c21e430f3bb70a6d2289
Ian <38480036+Iankimble@users.noreply.github.com>
2023-09-19 12:23:04 +0000

    class Activities 9/19/23

--- quizes/introQuiz1.py
+++ quizes/introQuiz1.py
@@ -1,33 +1,68 @@
 # 1. In your on words, describe what a variable is.
+"container data"
 
 # 2. Name three (3) operators we've learned about in class and describe 
 # a situation where you would use each one. 
+"arithmatic operators (addition, substraction, multiplication)"
+"logical operators (true or false) questions"
+"comparison operators (comparing values), ex. comparing fruit"
 
 # 3. Describe the difference between camal case and pascal case.
+"camel case- every word execpt the first is capilized"
+"pascal case- every word is capitalized."
 
-# 4. Which operator is being used in the example below
+NameOfClass= ""
+nameOfClass= ""
 
 # 5. You have been given a $100.00 dollar gift card from a family member. Another 
 # family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
- 
+moneyFromFamOne=100.00
+moneyFromFamTwo=50.00
+moneyFromFriend=35.50
+
+totalAmountOfMoney= 100.00 + 35.50+ 50.00
+print(totalAmountOfMoney)
+
+totalAmountOfMoneyVariables= moneyFromFamOne + moneyFromFamTwo + moneyFromFriend
+print(totalAmountOfMoneyVariables)
+
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
+name = input("what is your name? ")
+#print(name)
 
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
 # They want to know a visitor's full name, their age, if they have an allergy, and what their 
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
 # and use the input() function and print function to take in and print out the user's responses. 
- 
+name = input("what is full name?")
+age = input("how old are you?")
+allergy = input("do you have any allergies?")
+favoriteFood = input("What is your favorite food?")
+
+print(name)
+print(age)
+print(allergy)
+print(favoriteFood)
+
+print(name, age, allergy, favoriteFood)
+
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.
 # If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
-#  have an allergy they can eat the regular bread. What data type would best be used in this 
+# have an allergy they can eat the regular bread. What data type would best be used in this 
 # scenario and how would you write a variable to represent this value?
 
+"data type boolean"
+gluttenAllergy= ""
+HasGluttenAllergy= ""
 
 # 9. In your own words, describe what a program is?
+"set instructions that completes a specific task"
 
 # 10. Describe the difference between a float and an integer.
+"float = decimal"
+"integer = whole number; no decimal"
