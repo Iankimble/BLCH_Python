4c1009077b6cc12eee8909c0f101fe3aa410daf5
Ian <38480036+Iankimble@users.noreply.github.com>
2023-09-22 15:56:21 +0000

    activity answers

--- introPythonActivities/dataCastingActivity.py
+++ introPythonActivities/dataCastingActivity.py
@@ -1,19 +1,32 @@
-# convert the following datatypes into floats.
-12
-100
+# 1. convert the following data types into floats.
+number_one= 12
+number_two= 100
 
-# convert the following floats into integers.
-12.34
-23.33
+print(float(number_one))
+print(float(number_two))
 
-# modify the code below to print an integer number. 
+print(float(number_one),float(number_two))
+
+print(float(12))
+print(float(100))
+
+# 2. convert the following floats into integers.
+numberThree= int(12.34)
+numberFour= int(23.33)
+
+print(numberThree)
+print(numberFour)
+
+print('---------------------------------------')
+# 3. modify the code below to print an integer number. 
 num_one = 20
-num_two= 23.45
+num_two= int(23.45)
 
+print(int(num_one + num_two))
 print(num_one + num_two)
 
 # modify the following code below to work correctly. 
 name_of_pet = "Spuny"
-age_of_pet = 10
+age_of_pet = str(10) # '10'
 
-print("this is my dog " + name_of_pet + ". He is " + age_of_pet + " years old.")
+print("this is my dog, " + name_of_pet + " .He is " + age_of_pet + " years old.")
