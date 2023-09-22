2c89267d40ba5ec1602a98492df877adc882a41f
Ian <38480036+Iankimble@users.noreply.github.com>
2023-09-22 03:11:12 +0000

    function class work

---
+++ apPythonActivities/starterFunctions.py
@@ -0,0 +1,18 @@
+# define the function. 
+#You need to use the def keyword first
+def welcome_msg():
+    print('Welcome to the Python coding class.')
+
+# call the function by writing it again. 
+# This time around you dont need to write the instructions
+#  or the def key word because it's already be written.
+
+welcome_msg()
+
+
+# 
+def custom_welcome_msg(name):
+    print("Good Morning " + name +". Welcome to Python class.")
+    
+custom_welcome_msg('Ian')
+
--- introPythonActivities/demo.py
+++
@@ -1,2 +0,0 @@
-var name = "ian"
-console.log(name)
