2977e0f94e3598effd971b927a9ec96732b119e6
Ian <38480036+Iankimble@users.noreply.github.com>
2023-09-22 14:28:49 +0000

    data casting activity answers

--- apPythonActivities/dataCastingActivity.py
+++ apPythonActivities/dataCastingActivity.py
@@ -1,20 +1,43 @@
-# convert the following datatypes into floats.
-12
-100
+# type() function will return the data type of the value.
+numberOfCerealBoxes= "10.45"
+#print(type(numberOfCerealBoxes))
 
-# convert the following floats into integers.
+# 1. convert the following datatypes into floats.
+a=float(12)
+b=float(100)
+
+print(a)
+print(b)
+
+print(float(a))
+print(float(b))
+
+print(float(12))
+print(float(100))
+print("-----------------------------")
+# 2. convert the following floats into integers.
 12.34
 23.33
 
-# modify the code below to print an integer number. 
+print(int(12.34))
+print(int(23.33))
+
+print("---------------------------")
+# 3. modify the code below to print an integer number. 
 num_one = 20
 num_two= 23.45
 
 print(num_one + num_two)
+print(num_one+ int(num_two))
+#print(int(23.45))
+#print(num_one)
 
-# modify the following code below to 
+print(int(num_one + num_two))
+print("-----------------------------")
+# modify the following code below to work. 
 name_of_pet = "Spuny"
-age_of_pet = 10
+age_of_pet = str(10)
 
-print("this is my dog "+ name_of_pet + ". He is " + age_of_pet+ " years old.")
+print("----------------------------------")
+print("this is my dog "+ name_of_pet + ". He is " + age_of_pet + " years old.")
  
