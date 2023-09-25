5206125cb78de907f72b88e51b92f65d7afbf704
Ian <38480036+Iankimble@users.noreply.github.com>
2023-09-25 01:56:40 +0000

    folder structure

--- Intro_Python_Class/introPythonActivities/functionStarterActivity.py
+++ Intro_Python_Class/introPythonActivities/functionStarterActivity.py
@@ -14,3 +14,29 @@
 # what class they have next after this one. this code should use a 
 # variable to pass a value into the parameter of a function. The variable should
 # be real, user data- not hard coded data.  
+
+def isClassOver(timeParameter):
+    if timeParameter>11.30:
+        print('class is over. Time to go.')
+    else:
+        print('no, lets keep going')
+
+isClassOver(11.31)
+
+def writeName(nameParameter):
+    user_name= input('what is your name?')
+    if not isinstance(nameParameter, (int, float)): 
+        print('cannot be a number')
+    else:
+        print('hello ' + user_name)
+
+#writeName('Ian')
+
+def petNoise(petParameter):
+    if petParameter== 'dog':
+        print('woof woof')
+    elif petParameter =='cat':
+        print('meow')
+
+#petNoise('cat')
+
