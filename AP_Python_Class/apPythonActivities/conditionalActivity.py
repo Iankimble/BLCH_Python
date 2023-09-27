203048d05df625eae04abbba7716f422045f5821
Ian <38480036+Iankimble@users.noreply.github.com>
2023-09-27 15:11:39 +0000

    correcting numbers on lesson

--- AP_Python_Class/apPythonActivities/conditionalActivity.py
+++ AP_Python_Class/apPythonActivities/conditionalActivity.py
@@ -6,7 +6,7 @@
 
 # 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
 
-# 3. Write a conditional statement for food in the refridgerator.
+# 4. Write a conditional statement for food in the refridgerator.
 # your conditional statement should be wrapped in a function that takes one (1)
 # parameter. The data type for this parameter should be true or false. 
 # your function should have a line of code that asks the user if there is food in the refridgerator.
@@ -15,7 +15,7 @@
 # When you call your function there should be an argument that accepts 
 # a boolean. 
 
-# 4. Create a function that checks the inventory of cereal for a store. 
+# 5. Create a function that checks the inventory of cereal for a store. 
 # your function should parameter should accept an integer. In your function
 # use a conditional statement to determine if you need to order more cereal.
 # If there is more than 10 boxes, print "inventory full", else if there are less than 
