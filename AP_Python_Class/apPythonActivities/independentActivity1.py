84108ab545f4b02f59b7e80e4179418960890613
Ian <38480036+Iankimble@users.noreply.github.com>
2023-10-02 11:56:52 +0000

    independentActivity1 assignment

---
+++ AP_Python_Class/apPythonActivities/independentActivity1.py
@@ -0,0 +1,32 @@
+# read and review the following pages on Python lists. Use these to help you solve
+# the questions. 
+
+# Answers must be submitted by the end of class to recieve a grade. 
+
+# REMEMBER TO USE WRITE CLEAN AND READABLE CODE!
+
+
+linkOne= 'https://www.w3schools.com/python/python_lists.asp'
+linkTwo= 'https://afternerd.com/blog/python-lists-for-absolute-beginners/'
+
+# When ready, answer the following prompts
+
+# 1.Create a simple list variable that contains 5 list items. It is up to you what will be in your list. 
+# Some examples: favorite_atheletes, favorite_games, favorite_books, etc.  
+
+
+# 2. You have been hired by amazon to be an engineer. Your first assignment is to fix their
+# shopping cart function. Your goal is to create a line of code that will
+# allow users to enter the item they want as a string value, and add it to the items that
+# are already exist in the cart. Once the new item is entered, a list of all items in the cart should print out. 
+
+# HINT - for this function you will need to use the append() function. 
+
+shopping_cart = ['notebook', 'pens','tape','mousepad']
+
+
+# 3. Create a program that will only print out the odd numbers in this list. 
+
+# HINT- to solve this you will need to use the range() function. 
+
+number_list= [1,2,3,4,5,6,7,8,9,10]
