b96ea076944d46d6d19821cfde0ce2a06a4ed61f
Ian <38480036+Iankimble@users.noreply.github.com>
2023-10-03 11:49:59 +0000

    updating daily work

--- catchup/catcup_activity2.py
+++
@@ -1,5 +0,0 @@
-# 1. find a video or article on technology and how it impacts black people (it can be positive or negative)
-# write a 100 word report on what you read and 3 things you have learned. 
-
-# 2. research the development cycle of a typical web application. write a 500 word report 
-# breaking down the steps an app/ program is developed. 
