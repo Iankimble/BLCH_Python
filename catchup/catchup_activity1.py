0859d9d0db04915b83df27d09f9a7823c101b075
Ian <38480036+Iankimble@users.noreply.github.com>
2023-10-06 15:33:18 +0000

    EXTRA CREDIT

--- catchup/catchup_activity1.py
+++ catchup/catchup_activity1.py
@@ -1,8 +1,8 @@
-# 1. Explain what built-in functions are? Then describe and proivde a
-# code example of 3 built-in function we HAVE NOT learned about in class
+# 1. Explain what built-in functions are? Then describe and proivde 
+# code examples of three (3) built-in functions we learned about in class.
 
 
-# 2. provide 3 cod examples of how to use a conditional statement. you will need to use on of each of the following
+# 2. provide three (3) code examples of how to use a conditional statement. you will need to use one (1) of each of the following
 # operators in your examples: assignment operator, the logical operator and comparison operator.
 
 
