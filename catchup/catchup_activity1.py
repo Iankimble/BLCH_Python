fea8893fdd6dc89be6a58623cc7958eef1f6a7f9
Ian <38480036+Iankimble@users.noreply.github.com>
2023-10-06 15:30:34 +0000

    EXTRA CREDIT ASSIGNMENT 1

---
+++ catchup/catchup_activity1.py
@@ -0,0 +1,17 @@
+# 1. Explain what built-in functions are? Then describe and proivde a
+# code example of 3 built-in function we HAVE NOT learned about in class
+
+
+# 2. provide 3 cod examples of how to use a conditional statement. you will need to use on of each of the following
+# operators in your examples: assignment operator, the logical operator and comparison operator.
+
+
+# 3.Create function that will loop through the list of numbers provide below and multiply each number by 3
+list_of_numbers=[100,20,34,45,75,9,60]
+
+
+# 4. Create a function that will check is a user's input is a palendrome.
+# HINT1: a palendrome is a word that reads the same way backwards as it does forwards
+# HINT 2: you will need to use a loop.
+
+
