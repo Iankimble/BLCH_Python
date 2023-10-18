985ddd13c0e69b8c2bcb5799bebf847d7b026b8d
Ian <38480036+Iankimble@users.noreply.github.com>
2023-10-18 15:02:03 +0000

    while loop activity update

--- AP_Python_Class/loopReviewActivity.py
+++ AP_Python_Class/loopReviewActivity.py
@@ -1,12 +1,14 @@
+# Create while loops for the following conditions
 
-# Create a security camera program that uses a while loop to detect if there is an
+
+# 1. Create a security camera program that uses a while loop to detect if there is an
 # object in site. 
 
-# Create a Printer Loop program that will continue to print copies of a document based on the number
+# 2. Create a Printer Loop program that will continue to print copies of a document based on the number
 # that the user inputs. 
 
-# Create a Stop Light Loop that will change the light color based on different time intervals. 
+# 3. Create a Stop Light Loop that will change the light color based on different time intervals. 
 # every 30 seconds the light should change between green and red. 
 
-# BONUS: add an additional condition that will change the light to yellow for 5 seconds before the
+# 3. BONUS: add an additional condition that will change the light to yellow for 5 seconds before the
 # next light change. 
