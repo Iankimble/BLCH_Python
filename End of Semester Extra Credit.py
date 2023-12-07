16eea8aa15e4795c24ce99324b4fa661f37f037d
Ian <38480036+Iankimble@users.noreply.github.com>
2023-12-07 16:14:57 +0000

    end of semester credit

---
+++ End of Semester Extra Credit.py
@@ -0,0 +1,38 @@
+# Extra Credit # 1
+
+# PYTHON MOVIE GUESSING GAME
+
+# Your assignment is to create a movie guessing game using python. Users will be given clues as to what the movie is. 
+
+# Directions/ Requirements:
+# 1. Your program should give your user five attempts to guess the correct answer. 
+# 2. Each turn your guessing game should print out a clue as to what the movie answer is. 
+# 3. If the user guesses incorrectly, the program will output a message telling them they are incorrect, 
+# the number of attempts they have left, and then the next clue. 
+# 4. If the user exhausts all of their attempts, the program will inform them the game is over and reveal what the correct movie is. 
+# 5. If the user guesses the movie correctly, the program will congratulate the user for guessing the movie correctly. 
+
+# Helpful hints: your program will need to use the following:
+# - you will need to use a Python loop,
+# - you will need to use the input function to take in data.
+# - you will need to use Python conditional statements. 
+
+# Remember to take your time and break down the prompt into smaller pieces.
+
+
+# Extra Credit
+# PYTHON ROCK, PAPER, SCISCORS -BEST OF FIVE GAME
+
+# Your assignment is to develop a rock, paper, scicors game, where the first user must win three (3) rounds wins. 
+
+# Directions/ Requirements
+# 1. Your program should prompt the user to enter a number value that will represent rock, paper, and scissors. Your
+# program must inform the user of what each number represents
+# 2. When the user enters a value, the computer must randomly select another value, and then compare the values based on the
+# rock, paper, scissors format (paper beats rock, rock beats scissors, scissors beats paper)
+# 3. Your game must loop for five (5) rounds. If the computer wins 3 rounds, the user loses, if the user wins 3 wins, the user wins. 
+
+# BONUS: develop logic for sitations where the game ends in a tie. 
+
+
+
