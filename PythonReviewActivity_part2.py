4ebeaf393cfaddf0c4959b8f0fa7f0fb57933020
Ian <38480036+Iankimble@users.noreply.github.com>
2024-01-09 14:27:26 +0000

    python mad lib

---
+++ PythonReviewActivity_part2.py
@@ -0,0 +1,12 @@
+#  Winter break Python Mad libs 
+# Create a short mad lib story about your winter break. 
+# Your mad lib should include 6 input fields for users to enter data.
+# Your mad lib program should prompt the user to enter 2 nouns, 2 verbs, and 2 adjectives. 
+# Your mad lib should be written in complete sentences. 
+# Once completed, submit your madlib to your github repository by using the source control tool in codespaces. 
+
+
+
+
+
+
