abe89e029abf3b01dc6718b178eb86f462dfa99a
Ian <38480036+Iankimble@users.noreply.github.com>
2024-01-17 13:18:46 +0000

    daily assignment

---
+++ MONDAY Async Work/PythonReview_3.py
@@ -0,0 +1,4 @@
+# Use the concepts that we've learne in class to create and develop a simple hangman game.
+
+# Use the concepts that we've learned in class and create a function where when a user enters a string the function will return the number
+# of characters in that string.
--- PythonReviewActivity_part2.py
+++ PythonReviewActivity_part2.py
@@ -5,8 +5,14 @@
 # Your mad lib should be written in complete sentences. 
 # Once completed, submit your madlib to your github repository by using the source control tool in codespaces. 
 
+def madLib():
+    adj_1= input('please enter an adjective')
+    adj_2= input('please enter another adjective')
+    noun_1= input("please enter a noun ")
+    noun_2= input( "please enter another noun ")
+    verb_1= input('please enter a verb ')
+    verb_2= input('please enter another verb ')
 
+    print(f'My holiday was {adj_1}. I recieve a {noun_1} from my {adj_2} aunt. I also went to {noun_2}. it was a {adj_1} time.')
 
-
-
-
+madLib()
