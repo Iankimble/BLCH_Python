b0a5250750dfa2151a2f26375d477799b48f8501
Ian <38480036+Iankimble@users.noreply.github.com>
2024-01-18 15:04:04 +0000

    daily activity - extra credit

--- MONDAY Async Work/PythonReview_3.py
+++ MONDAY Async Work/PythonReview_3.py
@@ -1,4 +1,7 @@
-# Use the concepts that we've learne in class to create and develop a simple hangman game.
+# Use the concepts that we've learned in class to create and develop a 
+#simple hangman game.
 
-# Use the concepts that we've learned in class and create a function where when a user enters a string the function will return the number
+# Use the concepts that we've learned in class and create a 
+# function where when a user enters a string the function will return the number
 # of characters in that string.
+
