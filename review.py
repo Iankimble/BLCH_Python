f35a8a0b600aa0130a8b6a5e7662d32fcfd397f9
Ian <38480036+Iankimble@users.noreply.github.com>
2024-01-24 16:37:10 +0000

    review extra credit work

---
+++ review.py
@@ -0,0 +1,22 @@
+# Sve this as pythonReview.py
+
+# In your own words describe, what a computer program is?
+
+# In your own words, describe what it mean to call a function? 
+
+# Create a elevator function that will run specific lines of code 
+# based on the conditions provided by a user. If the user types in 101,
+# the function should print out they are going to the boys latin office, 
+# if they type in 203, they are going to the gym, 
+# and if they type in the letter g, the lobby. else, 
+# if the input doesnt match any of the values provided, 
+# tell the user that floor doesn't exist and to please
+# enter a valid floor number.
+
+# Create a function that will do the following calculation.
+# Your function should take in three argument. it should add the first
+# two arguments and then the sum of the first two (2) should be divided 
+# by the third argument. You function should then print the result.
+
+
+
